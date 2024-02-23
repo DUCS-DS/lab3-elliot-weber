c4a23f69fff984dfdabf4d89814aca61759d37b4
elliotweber753 <elliot.weber@griffithuni.edu.au>
2024-02-23 10:10:56 -0600

    Elliot Weber Lab Finished

--- finite.py
+++ finite.py
@@ -1,20 +1,55 @@
+#elliot weber
+from genfinite import lst
 from llist import LList, Node, append
 
 
+
+
+
 def length(lst):
-    """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
+
+
+    counter =1
+    if lst.head:
+        node = lst.head
+        while node.next:
+            node=node.next
+            counter=counter+1
+    else:
+        return "no"
+    return counter
 
 
 def llprint(lst):
-    """print a finite linked list"""
-    pass  # delete this line when you add your code
+    if lst.head: 
+        node = lst.head
+        while node.next:
+            node=node.next
+            print(node.val)
+    
+    else:
+        print("no")
+
+
 
 
 if __name__ == "__main__":
 
-    pass  # delete this line when you add your code below
 
-    #
-    # your tests go here
-    #
+    llist=LList()
+
+    append(llist,Node(1))
+    append(llist,Node(2))
+    append(llist,Node(4))        
+    append(llist,Node(8))
+    append(llist,Node(16))
+    append(llist,Node(32))
+    append(llist,Node(64))
+    append(llist,Node(128))
+    append(llist,Node(256))
+    append(llist,Node(512))
+
+    
+
+    print(length(lst))
+    llprint(llist)
